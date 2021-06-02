c31438f248bd7323ae77aeac25ef8eef6e58a3c5
Zbigniew Zolnierowicz <zbigniew.zolnierowicz@gmail.com>
2021-06-03 01:08:48 +0200

    Add a grid builder

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -3,10 +3,11 @@ defmodule Identicon do
     input
       |> hash_input
       |> pick_color
+      |> build_grid
   end
 
-  def hash_input(input_string) do
-    hex = :crypto.hash(:md5, input_string)
+  def hash_input(input) do
+    hex = :crypto.hash(:md5, input)
     |> :binary.bin_to_list
 
     %Identicon.Image{hex: hex}
@@ -17,5 +18,23 @@ defmodule Identicon do
   ) do
     [r, g, b | _tail] = hex
     %Identicon.Image{image | color: %Identicon.Color{r: r, g: g, b: b}}
+  end
+
+  def build_grid(
+    %Identicon.Image{hex: hex} = image
+  ) do
+    grid_data = hex
+      |> Enum.chunk_every(3, 3, :discard)
+      |> Enum.map(&mirror_row/1)
+      |> List.flatten
+      |> Enum.with_index
+
+    %Identicon.Image{image | hex: grid_data}
+  end
+
+  def mirror_row(
+    [first, second | _tail] = row
+  ) do
+    row ++ [second, first]
   end
 end
