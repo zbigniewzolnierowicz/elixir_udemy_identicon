545c748c2e05833121589682f6eb43b762caa4d1
Zbigniew Zolnierowicz <zbigniew.zolnierowicz@gmail.com>
2021-06-03 01:32:23 +0200

    Add a pixel map builder

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -5,6 +5,7 @@ defmodule Identicon do
       |> pick_color
       |> build_grid
       |> filter_odd_squares
+      |> build_pixel_map
   end
 
   def hash_input(input) do
@@ -42,5 +43,17 @@ defmodule Identicon do
   def filter_odd_squares(%Identicon.Image{grid: grid} = image) do
     filtered_grid = Enum.filter(grid, fn({item, _index}) -> rem(item, 2) == 0 end)
     %Identicon.Image{image | grid: filtered_grid}
+  end
+
+  def build_pixel_map(%Identicon.Image{grid: grid} = image) do
+    pixel_map = Enum.map grid, fn({_item, index}) ->
+      horizontal = rem(index, 5) * 50
+      vertical = div(index, 5) * 50
+      start_point = {horizontal, vertical}
+      end_point = {horizontal + 50, vertical + 50}
+      {start_point, end_point}
+    end
+
+    %Identicon.Image{image | pixel_map: pixel_map}
   end
 end
--- lib/image.ex
+++ lib/image.ex
@@ -1,5 +1,6 @@
 defmodule Identicon.Image do
   defstruct hex: nil,
     grid: nil,
+    pixel_map: nil,
     color: %Identicon.Color{r: 255, g: 255, b: 255}
 end
